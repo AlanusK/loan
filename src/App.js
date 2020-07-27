26c969f0dadeefa70eb0cd0da60958b19a5d03ad
Alan Kajara <akajara@gmail.com>
2020-07-27 22:33:11 +0300

    Complete header commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './App.css';
+import logo from './logo.svg';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <img src={logo} alt="logo" className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1" >LOAN REPAYMENT MANAGEMENT SYSTEM</Menu.Item>
+          <Menu.Item key="2" style={{marginLeft: '590px'}}>Log In</Menu.Item>
+          <Menu.Item key="3" style={{float: 'right'}}>| Create Account</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Loan repayment management system ©2020 Created by Abk</Footer>
+    </Layout>
   );
 }
 
-export default App;
+export default App; 
