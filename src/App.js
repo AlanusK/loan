7d7289acc6ea7408c86dcf7cbca795134d17e05b
Alan Kajara <akajara@gmail.com>
2020-08-03 23:21:44 +0300

    Start new loan (new App.js & App.css file), save previous file as App1.js & App1.css

--- src/App.js
+++ src/App.js
@@ -1,65 +1,53 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Breadcrumb, Button } from 'antd';
 import './App.css';
-import logo from './logo.svg';
-import Home from './Home.js';
-import Account from './Account.js';
-import LogIn from './LogIn.js';
-import Customer from './Customer.js';
-import Loan from './Loan.js';
-
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+  UserOutlined,
+  DollarCircleOutlined,
+  CreditCardOutlined
+} from '@ant-design/icons';
 
-
-
-const { Header, Content, Footer } = Layout;
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <Router>
-      <Layout className="layout">
-        <Header>
-          <img src={logo} alt="logo" className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1"><Link to="/">LOAN REPAYMENT MANAGEMENT SYSTEM</Link></Menu.Item>
-            <Menu.Item key="2" style={{ float: 'right' }}><Link to="/Account">| Create Account</Link></Menu.Item>
-            <Menu.Item key="3" style={{ float: 'right' }}><Link to="/LogIn">Log In</Link></Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>About</Breadcrumb.Item>
-            <Breadcrumb.Item>Contact</Breadcrumb.Item>
-          </Breadcrumb>
-          <Switch>
-            <Route path="/Account">
-              <Account />
-            </Route>
-            <Route path="/LogIn">
-              <LogIn />
-            </Route>
-            <Route path="/Customer">
-              <Customer />
-            </Route>
-            <Route path="/Loan">
-              <Loan />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Loan repayment management system ©2020 Created by Abk</Footer>
-      </Layout>
-    </Router>
+    <Layout className='layout'>
+      <Header className='header'>
+        <Breadcrumb className='breadcrumb'>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="./appC.js">Customer</a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="./appL.js">Customer View</a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Add Customer</Breadcrumb.Item>
+        </Breadcrumb>
+        <UserOutlined className='logo' />
+      </Header>
+
+      <Content className='content'>
+        <div>
+          <Button>
+            <UserOutlined className='icon'  />
+            <p>Customer</p>
+          </Button>
+          <Button>
+            <DollarCircleOutlined className='icon' />
+            <p>Loan</p>
+          </Button>
+          <Button>
+            <CreditCardOutlined className='icon'  />
+            <p>Payment</p>
+          </Button>
+        </div>
+      </Content>
+
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
 export default App;
+
---
+++ src/appC.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function appC() {
+  return (
+    <h1>first hierachy</h1>
+  );
+}
+
+export default appC;
